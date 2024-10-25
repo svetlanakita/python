b4dbf209cddd15ac24fef30cb85e2dc43d445174
svetlanakita <squpex@gmail.com>
2024-10-25 19:40:27 +0300

    Add files via upload

---
+++ task.py
@@ -0,0 +1,11 @@
+SECONDS_IN_MINUTE = 60  # количество секунд в минуте
+SECONDS_IN_HOUR = 60 * 60  # количество секунд в часе
+SECONDS_IN_DAY = 24 * SECONDS_IN_HOUR
+
+total_seconds_in_two_minutes = 2 * SECONDS_IN_MINUTE  # количество секунд в двух минутах
+total_seconds_in_five_hours = 5 * SECONDS_IN_HOUR  # количество секунд в пяти часах
+total_seconds_in_ten_hours = 10 * SECONDS_IN_HOUR
+
+print("Количество секунд в двух минутах:", total_seconds_in_two_minutes)
+print("Количество секунд в пяти часах:", total_seconds_in_five_hours)
+print("Количество секунд в десяти часах:", total_seconds_in_ten_hours)
